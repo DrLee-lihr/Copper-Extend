8b37baf3c6563f50be6ba82248f7562acaebfa4d
DrLee-lihr <812403lihr@sina.com>
2021-11-13 17:39:16 +0800

    dev

--- src/main/java/com/drleelihr/copper_extend/CopperExtendMod.java
+++ src/main/java/com/drleelihr/copper_extend/CopperExtendMod.java
@@ -11,17 +11,12 @@ import org.apache.logging.log4j.Logger;
 
 
 public class CopperExtendMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("builtin_wiki_viewer");
 	public static final Item CopperSword = new Item(new FabricItemSettings().group(ItemGroup.COMBAT));
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		Registry.register(Registry.ITEM,new Identifier("tutorial", "fabric_item"),CopperSword);
+		Registry.register(Registry.ITEM,
+				new Identifier("copper_extend", "copper_sword"),CopperSword);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
